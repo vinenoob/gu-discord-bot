8ea8aee500f8f60f52497b0c61c3b8e740b77a8b
JONATHAN NIELSEN <vinenoob@gmail.com>
2019-11-02 14:35:17 -0600

    Changes

--- bot/interface.py
+++ bot/interface.py
@@ -3,6 +3,8 @@ import os
 from discord.ext.commands import Bot
 from discord.ext import commands
 
+startup_extentions = ["test"]
+
 bot_prefix = "?"
 client = commands.Bot(command_prefix = bot_prefix)
 
@@ -10,15 +12,15 @@ client = commands.Bot(command_prefix = bot_prefix)
 async def on_ready():
     print("Bot")
 
-@client.command()
-async def helloworld(): #activated by "?helloworld"
-    print("hello world")
+@client.command(pass_context = True)
+async def hello(ctx): #activated by "?helloworld"
+    print("hello")
+    await ctx.send("hello")
 
 @client.event
 async def on_message(message):
     print(message)
     await client.process_commands(message) #ensure doesn't mess with other commands
-
 
 os.chdir("..")
 keyFile = open("gu-discordbot\\DONTSHOW\\key.txt", "r")
