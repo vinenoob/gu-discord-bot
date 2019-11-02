90dffe1e98f6b783209434cbcc1a2e20e64506e6
JONATHAN NIELSEN <vinenoob@gmail.com>
2019-11-01 23:47:52 -0600

    setup basic stuff

--- bot/interface.py
+++ bot/interface.py
@@ -1,11 +1,27 @@
 import discord
+import os
 from discord.ext.commands import Bot
 from discord.ext import commands
 
-client = discord.Client()
+bot_prefix = "?"
+client = commands.Bot(command_prefix = bot_prefix)
 
 @client.event
 async def on_ready():
     print("Bot")
 
-client.run("")
+@client.command()
+async def helloworld(): #activated by "?helloworld"
+    print("hello world")
+
+@client.event
+async def on_message(message):
+    print(message)
+    await client.process_commands(message) #ensure doesn't mess with other commands
+
+
+os.chdir("..")
+keyFile = open("gu-discordbot\\DONTSHOW\\key.txt", "r")
+key = keyFile.read()
+keyFile.close()
+client.run(key)
