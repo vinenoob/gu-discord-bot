83ad0fc64637b675ea5c659ea82096e231471c22
JONATHAN NIELSEN <vinenoob@gmail.com>
2019-11-03 13:36:02 -0700

    Improved I'm dad

--- bot/dad.py
+++ bot/dad.py
@@ -1,13 +1,13 @@
 from urllib import request
 import json
 
-toWatch = ["i am", "i'm", "im"] #all the things to respond to
+toWatch = ["i am ", "i'm ", "im "] #all the things to respond to
 def daddy(message :str):
     dadMessage = ""
     for watch in toWatch:
         if watch in message.lower(): #if we find what we are looking for
             start = message.find(watch) #find where the daddism starts
-            name = message[start+len(watch)+1:] #find the name of the idiot who dared say "I am" or similar
+            name = message[start+len(watch):] #find the name of the idiot who dared say "I am" or similar
             dadMessage = "Hi " + name + ", I'm dad!"
             return (True, dadMessage)
     return (False, dadMessage)
