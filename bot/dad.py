887eba606b7a804d11f9ad4c6d4a30d04ab717a1
JONATHAN NIELSEN <vinenoob@gmail.com>
2019-11-02 23:32:22 -0600

    Making things look nice

--- bot/dad.py
+++ bot/dad.py
@@ -1,11 +1,10 @@
-
-toWatch = ["i am", "i'm", "im"]
+toWatch = ["i am", "i'm", "im"] #all the things to respond to
 def daddy(message :str):
     dadMessage = ""
     for watch in toWatch:
-        if watch in message.lower():
-            start = message.find(watch)
-            name = message[start+len(watch)+1:]
+        if watch in message.lower(): #if we find what we are looking for
+            start = message.find(watch) #find where the daddism starts
+            name = message[start+len(watch)+1:] #find the name of the idiot who dared say "I am" or similar
             dadMessage = "Hi " + name + ", I'm dad!"
             return (True, dadMessage)
     return (False, dadMessage)
