ccd83abdf5af8c27b5810cd4e67eb34fd8176d7e
JONATHAN NIELSEN <vinenoob@gmail.com>
2019-11-04 22:38:11 -0700

    added commonGames

--- bot/gameList/interface.py
+++ bot/gameList/interface.py
@@ -22,3 +22,15 @@ class GameList(commands.Cog):
         person = self.bot.get_user(int(id))
         games = logic.gameList(person.name)
         await ctx.send(games)
+    
+    @commands.command(name="commonGames")
+    async def commonGames(self, ctx, *, people :str):
+        peopleList = people.split(",")
+        nameList = []
+        for id in peopleList:
+            if "<@!" in id: #see if the channel is a channel mention, ie "#but-stuff"
+                id = id[id.find("<@!") + len("<@!"):] #erase the first bit
+                id = id[:id.find(">")] #erase the last bit
+            person = self.bot.get_user(int(id))
+            nameList.append(person.name)
+        await ctx.send(logic.commonGames(nameList))
--- bot/gameList/logic.py
+++ bot/gameList/logic.py
@@ -1,5 +1,5 @@
 import os
-fileLoc = "gu-discord-bot\\bot\\gameList\\"
+fileLoc = "bot\\gameList\\"
 def addGame(person :str, game :str):
     fileName = fileLoc + person + ".txt"
     with open(fileName, "a+") as file:
@@ -11,5 +11,24 @@ def gameList(person :str):
     with open(fileName, "r") as gamesFile:
         for line in gamesFile:
             games += line
-            
     return games
+
+def commonGames(people :list):
+    gamesInCommon = {""}
+    firstPerson = True
+    for person in people:
+        fileName = fileLoc + person + ".txt"
+        personGames = {""}
+        personGames.clear()
+        with open(fileName, "r") as gamesFile:
+            for line in gamesFile:
+                if line[len(line)-1:] == "\n":
+                    personGames.add(line.lower()[:len(line)-1]) #set lower, delete newline
+                else:
+                    personGames.add(line.lower())
+        if firstPerson:
+            gamesInCommon = personGames
+            firstPerson = False
+        else:
+            gamesInCommon = gamesInCommon.intersection(personGames)
+    return str(gamesInCommon)
