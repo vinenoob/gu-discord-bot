6a86f9e108b9bbd6d883c9d05c9e16b0260bd35f
JONATHAN NIELSEN <vinenoob@gmail.com>
2019-11-05 19:24:37 -0700

    Fixed descriptions of gamelist functions

--- bot/gameList/interface.py
+++ bot/gameList/interface.py
@@ -6,14 +6,14 @@ class GameList(commands.Cog):
         self.bot = bot
 
     @commands.command(name="addGame")
-    '''Add a game to your game list'''
     async def addGame(self, ctx, *,game :str):
+        '''Add a game to your game list'''
         logic.addGame(ctx.author.name,game)
         await ctx.send("Game added!")
 
     @commands.command(name="addGames")
-    '''Add some games to your game list'''
     async def addGames(self, ctx, *, games:str):
+        '''Add some games to your game list'''
         for game in games.split(", "):
             logic.addGame(ctx.author.name,game)
         await ctx.send("Games added!")
@@ -23,8 +23,8 @@ class GameList(commands.Cog):
     #     print("Cog Example Listening")
     
     @commands.command(name="gameList")
-    '''Find out what games someone has on their list'''
     async def gameList(self, ctx, id :str):
+        '''Find out what games someone has on their list'''
         if "<@!" in id: #see if the channel is a channel mention, ie "#but-stuff"
             id = id[id.find("<@!") + len("<@!"):] #erase the first bit
             id = id[:id.find(">")] #erase the last bit
@@ -33,8 +33,8 @@ class GameList(commands.Cog):
         await ctx.send(games)
     
     @commands.command(name="commonGames")
-    '''Find common games among multiple peoples games list'''
     async def commonGames(self, ctx, *, people :str):
+        '''Find common games among multiple peoples games list'''
         peopleList = people.split(",")
         nameList = []
         for id in peopleList:
