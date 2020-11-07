d2352d3db3f3a87f788a6dd5d0e118fcddaeb6e7
Jonathan Nielsen <vinenoob@gmail.com>
2020-11-06 21:34:23 -0700

    fixed the fix

--- bot/Heck/logic.py
+++ bot/Heck/logic.py
@@ -1,11 +1,12 @@
 from urllib import request
 import json
 
-toWatch = [" heck ", " hell ", " darn ", " david ", " heckin ", " fack ", " scheiss ",] #all the things to respond to
+toWatch = ["heck", "hell", "darn", " david", "heckin", "fack", "scheiss",] #all the things to respond to
 def heckin(message :str):
     holy = ""
     for watch in toWatch:
         if watch in message.lower(): #if we find what we are looking for
+            print(watch + " vs " + message.lower())
             holy = "Not in my christian minecraft server :sunglasses: "
             return (True, holy)
     return (False, holy)
