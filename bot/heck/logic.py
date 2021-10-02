b49166f46eb965a4c9370c79fd91f24f9981458a
Jonathan Nielsen <vinenoob@gmail.com>
2021-10-01 21:28:12 -0600

    Fixed "your" detection

--- bot/heck/logic.py
+++ bot/heck/logic.py
@@ -18,30 +18,31 @@ YOUR_SPELLINGS = ["your", "youre"]
 YOUR_LIST = ["your", "you're"]
 def your(message: str):
     for watch in YOUR_LIST:
-        if watch in message:
-            yorResponse = list(random.choice(YOUR_SPELLINGS))
-            addedApostrophe = False
-            for i, letter in enumerate(yorResponse):
-                print(letter)
-                if not addedApostrophe:
-                    appostropheRNG = random.randint(i, len(yorResponse)-1)
-                    if appostropheRNG == i:
-                        yorResponse.insert(i, "'")
-                        addedApostrophe = True
+        for word in message.split():
+            if watch == word.lower():
+                yorResponse = list(random.choice(YOUR_SPELLINGS))
+                addedApostrophe = False
+                for i, letter in enumerate(yorResponse):
+                    print(letter)
+                    if not addedApostrophe:
+                        appostropheRNG = random.randint(i, len(yorResponse)-1)
+                        if appostropheRNG == i:
+                            yorResponse.insert(i, "'")
+                            addedApostrophe = True
 
-                deleteRNG = random.randint(0, 6)
-                if deleteRNG == 0:
-                    yorResponse.pop(i)
-                    
-            scrambleRNG = random.randint(0, 1)
-            if scrambleRNG == 0:
-                firstLetterPos = random.randint(0, len(yorResponse)-1)
-                secondLetterPos = firstLetterPos + 1
-                if secondLetterPos == len(yorResponse):
-                    secondLetterPos = firstLetterPos - 1
-                yorResponse[firstLetterPos], yorResponse[secondLetterPos] = yorResponse[secondLetterPos], yorResponse[firstLetterPos]
+                    deleteRNG = random.randint(0, 6)
+                    if deleteRNG == 0:
+                        yorResponse.pop(i)
+                        
+                scrambleRNG = random.randint(0, 1)
+                if scrambleRNG == 0:
+                    firstLetterPos = random.randint(0, len(yorResponse)-1)
+                    secondLetterPos = firstLetterPos + 1
+                    if secondLetterPos == len(yorResponse):
+                        secondLetterPos = firstLetterPos - 1
+                    yorResponse[firstLetterPos], yorResponse[secondLetterPos] = yorResponse[secondLetterPos], yorResponse[firstLetterPos]
 
-            return True, "".join(yorResponse) + "*"
+                return True, "".join(yorResponse) + "*"
     return False, ""
 if __name__ == "__main__":
     for i in range(100):
